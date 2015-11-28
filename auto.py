12439c8eac4e4205aa269f9583ce3dad6ec50850
jim_jhang <jingfu99@gmail.com>
2015-11-29 00:21:21 +0800

    2015-11-29

---
+++ auto.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+__author__ = 'jjj'
+# -*- coding:utf8 -*-
+import subprocess
+from datetime import date
+import shlex
+datestring = str(date.today())
+
+class Auto:
+    # make some difference everyday
+    def make_some_different(self):
+        with open("./shit", "a") as f:
+            f.write(datestring)
+    def main(self):
+        self.make_some_different()
+        cmd = "git add -u"
+        subprocess.Popen(shlex.split(cmd))
+        cmd = "git commit -m '{0}'".format(datestring)
+        subprocess.Popen(shlex.split(cmd))
+        cmd = "git push origin master"
+
+if __name__ == "__main__":
+    fucker = Auto()
+    fucker.main()
