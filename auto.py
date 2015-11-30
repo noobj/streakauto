6a4b5d4fc688b01200df186eeb2c516430c18051
jim_jhang <jingfu99@gmail.com>
2015-11-30 14:21:27 +0800

    2015-11-30

--- auto.py
+++ auto.py
@@ -4,6 +4,7 @@ __author__ = 'jjj'
 import subprocess
 from datetime import date
 import shlex
+import time
 datestring = str(date.today())
 
 class Auto:
@@ -15,8 +16,10 @@ class Auto:
         self.make_some_different()
         cmd = "git add -u"
         subprocess.Popen(shlex.split(cmd))
+	time.sleep(1)
         cmd = "git commit -m '{0}'".format(datestring)
         subprocess.Popen(shlex.split(cmd))
+	time.sleep(1)
         cmd = "git push origin master"
         subprocess.Popen(shlex.split(cmd))
 
